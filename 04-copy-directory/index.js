144ed4b66afab71c9214bbb56f5896afdf27594e
Svetlana Pozina <cherryblack@list.ru>
2022-05-17 01:33:09 +0300

    feat: add task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,43 @@
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
 
+const dirCopyName = path.join(__dirname, 'files-copy');
+const dirName = path.join(__dirname, 'files');
+
+async function createDir(dirCopyName) {
+  await fsPromises.mkdir(dirCopyName, {recursive: true});
+}
+
+async function removeDir(dirCopyName) {
+  try {
+    await fsPromises.rm(dirCopyName, {force: true, recursive: true});
+    func(dirName, dirCopyName);  
+    return;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const func = async(dirName, dirCopyName) => {
+  try {
+    createDir(dirCopyName);
+    const files = await fsPromises.readdir(dirName, { withFileTypes: true });
+    for (const file of files) {
+      const fileString = path.join(dirName, file.name);
+      const fileCopyString = path.join(dirCopyName, file.name);
+      if (file.isFile())
+      {
+        fs.copyFile(fileString, fileCopyString, (err) => {if (err) throw err;});
+      }
+      else {
+        func(fileString, fileCopyString);
+      }
+    }
+    return;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+removeDir(dirCopyName);
